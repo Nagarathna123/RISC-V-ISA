8bf5ac38d2df555b1affe24df0ae56bed973b4a1
Nagarathna123 <nagarathna.nayak.1994@gmail.com>
2016-02-20 00:50:36 +0530

    Update Single_Cycle_Processor.v

--- Single_Cycle_Processor.v
+++ Single_Cycle_Processor.v
@@ -20,8 +20,9 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 `include "pc_block.v"
-`include "fetch.v"
+`include "Dist_RAM.v"
 `include "adder.v"
+`include "dist_ROM.v"
 `include "reg_file.v"
 `include "sz_ex.v"
 `include "ctrl.v"
@@ -42,10 +43,10 @@ module Single_Cycle_Processor(
 	 
 	 // Instantiation of PC_BLOCK
     wire [`REGISTER_WIDTH-1:0] next_addr_t ;
-    reg [`REGISTER_WIDTH-1:0] curr_addr_t ;
+    wire [`REGISTER_WIDTH-1:0] curr_addr_t ;
 	 
-    pc_block  pc_bloc_t ( .rst_t(rst), .clk_t(clk) ,
-                       .next_addr_t(next_addr) , .curr_addr_t(curr_addr));
+    pc_block  pc_bloc_t ( .rst(rst_t), .clk(clk_t) ,
+                       .next_addr(next_addr_t) , .curr_addr(curr_addr_t));
   
     //Instantiation of fetch module
    // reg [`REGISTER_WIDTH-1:0] Instruction ;
@@ -54,46 +55,47 @@ module Single_Cycle_Processor(
 //	             .Instruction(InstructionRegister));
 
 //Instantiation of distributed ROM 
-
-   dist_ROM dist_ROM_t (.curr_addr_t(a), . Instruction(spo));
+   wire [`REGISTER_WIDTH-1:0] Instruction ;
+   dist_ROM dist_ROM_t (.a(curr_addr_t), . spo(Instruction));
  
     //Instantiation of adder
 	 reg [`REGISTER_WIDTH-1:0] val_2_PC_adder = 32'd4;
-	 reg [`REGISTER_WIDTH-1:0] Add_4_Out_t;
+	 wire [`REGISTER_WIDTH-1:0] Add_4_Out_t;
 	 
-	 adder adder_t (. curr_addr_t( val_1) , .val_2_PC_adder(val_2) , .Add_4_Out_t(out));
+	 adder adder_t (. val_1(curr_addr_t) , .val_2(val_2_PC_adder) , .out(Add_4_Out_t));
 
 
     //Instantiation of Control Unit 
-    reg [(`ALU_CTRL_WIDTH-1):0] alu_ctrl_t ;	
-    reg reg_file_wr_en_t , d_mem_rd_en_t , alu_op2_sel_t , d_mem_wr_en_t;
-    reg [1:0] reg_file_wr_back_sel_t ;
-	 reg [1:0] d_mem_size_t;
-	 reg jal_t , jalr_t ;
+    wire [(`ALU_CTRL_WIDTH-1):0] alu_ctrl_t ;	
+    wire reg_file_wr_en_t , d_mem_rd_en_t  , d_mem_wr_en_t;
+    wire [1:0] reg_file_wr_back_sel_t ;
+	 wire [1:0] d_mem_size_t;
+	 wire jal_t , jalr_t , alu_op2_sel_t;
 	 
-	 ctrl ctrl_t ( .Instruction(inst) , .jal_t(jal) , .jalr_t(jalr_t) , .d_mem_size_t(d_mem_size) ,
-	               .reg_file_wr_back_sel_t(reg_file_wr_back_sel) , .d_mem_wr_en_t(d_mem_wr_en) ,
-		       .alu_op2_sel_t(alu_op2_sel) , .d_mem_rd_en_t(d_mem_rd_en) , .reg_file_wr_en_t(reg_file_wr_en),
-		       .alu_ctrl_t(alu_ctrl) ) ;
+	 ctrl ctrl_t ( .inst(Instruction) , .jal(jal_t) , .jalr(jalr_t) , .d_mem_size(d_mem_size_t) ,
+	               .reg_file_wr_back_sel(reg_file_wr_back_sel_t) , .d_mem_wr_en(d_mem_wr_en_t) ,
+		       .alu_op2_sel(alu_op2_sel_t) , .d_mem_rd_en(d_mem_rd_en_t) , .reg_file_wr_en(reg_file_wr_en_t),
+		       .alu_ctrl(alu_ctrl_t) ) ;
 						
 						
     //Instantiation of register file 
-	 reg [`REGISTER_WIDTH-1:0] reg_data_1_t , reg_data_2_t , wr_reg_data_t;
+	 wire [`REGISTER_WIDTH-1:0] reg_data_1_t , reg_data_2_t ;
+    reg	[`REGISTER_WIDTH-1:0] wr_reg_data_t;
 	 //wire wr_en_t ==  reg_file_wr_en_t ;
 	 wire [(`REG_INDEX_WIDTH-1):0] rd_reg_index_1_t = Instruction[`REG1_MSB :`REG1_LSB] ;
 	 wire [(`REG_INDEX_WIDTH-1):0] rd_reg_index_2_t= Instruction[`REG2_MSB :`REG2_LSB] ;
 	 wire [(`REG_INDEX_WIDTH-1):0] wr_reg_index_t = Instruction[`DST_REG_MSB :`DST_REG_LSB] ;
 	 
-	 reg_file reg_file_t ( .reg_data_1_t(reg_data_1) , .reg_data_2_t(reg_data_2) ,
-                               .rst_t(rst) , .clk_t(clk) , .reg_file_wr_en_t(wr_en) ,.rd_reg_index_1_t(rd_reg_index_1) ,
-                               .rd_reg_index_2_t(rd_reg_index_2) , .wr_reg_index_t(wr_reg_index) , 
-                               .wr_reg_data_t(wr_reg_data));	
+	 reg_file reg_file_t ( .reg_data_1(reg_data_1_t) , .reg_data_2(reg_data_2_t) ,
+                               .rst(rst_t) , .clk(clk_t) , .wr_en(reg_file_wr_en_t) ,.rd_reg_index_1(rd_reg_index_1_t) ,
+                               .rd_reg_index_2(rd_reg_index_2_t) , .wr_reg_index(wr_reg_index_t) , 
+                               .wr_reg_data(wr_reg_data_t));	
 								 
 								 
 	 // Instantiation of sign extend module
-	 reg [(`OPERAND_WIDTH - 1):0] sz_ex_val_t ;
+	 wire [(`OPERAND_WIDTH - 1):0] sz_ex_val_t ;
 	 
-    sz_ex sz_ex_t ( .Instruction(inst) , .sz_ex_val_t(sz_ex_val));
+    sz_ex sz_ex_t ( .inst(Instruction) , .sz_ex_val(sz_ex_val_t));
 	 
 	 
 	  // Implementation of 2:1 MUX 
@@ -108,28 +110,30 @@ module Single_Cycle_Processor(
 	  end
 	 
 	 // Implementation OF ALU 
-	 reg [`REGISTER_WIDTH :0 ] ALU_Out_t;
-	 reg bcond_t ;
-	 Exec Exec_t ( .Operand1_t(Operand1) , .Operand2_t(Operand2) , .ALU_Out_t(Out) , 
-	               .alu_ctrl_t(Operation) ,.bcond_t(bcond) );
+	 wire [`REGISTER_WIDTH :0 ] ALU_Out_t;
+	 wire bcond_t ;
+	 Exec Exec_t ( .Operand1(Operand1_t) , .Operand2(Operand2_t) , .Out(ALU_Out_t) , 
+	               .Operation(alu_ctrl_t) ,.bcond(bcond_t) );
 	 
 	 // Implementation of 2:4 mux 
 	 
 	 always @ ( reg_file_wr_back_sel_t )
 	  begin 
-	   case ( reg_file_wr_back_sel_t )
-		 
-		   2'b00 : wr_reg_data_t = ALU_Out_t ;
-		   2'b01 : wr_reg_data_t = d_mem_rd_data_t ; ///Should define
-		   2'b10 : wr_reg_data_t = Add_4_Out_t ;
-		   2'b11 : wr_reg_data_t = Add_Out_t ;
+	   if( reg_file_wr_back_sel_t== 2'b00 )
+		     wr_reg_data_t = ALU_Out_t ;
+			 else if(reg_file_wr_back_sel_t== 2'b01)
+		          wr_reg_data_t = d_mem_rd_data_t ; ///Should define
+					 else if (reg_file_wr_back_sel_t== 2'b10)
+		               wr_reg_data_t = Add_4_Out_t ;
+							else
+	                    wr_reg_data_t = Add_Out_t ;
 			
-		endcase
+		
 	  end
     
 	// Implementation of simple adder 
-	reg [`REGISTER_WIDTH-1:0] Add_Out_t;
-	adder adder_1_t (.curr_addr_t( val_1) , .sz_ex_val_t(val_2) , .Add_Out_t(out) );
+	wire [`REGISTER_WIDTH-1:0] Add_Out_t;
+	adder adder_1_t (.val_1(curr_addr_t) , .val_2(sz_ex_val_t) , .out(Add_Out_t ));
 	
 	//Implementation of OR gate 
 	wire pc_mux1_sel_t  ;
@@ -146,7 +150,7 @@ module Single_Cycle_Processor(
 		end
 		
 	 //Implementation of pc_mux_2	
-	 reg pc_mux2_Out_t ;
+	 reg [`REGISTER_WIDTH-1 : 0 ] pc_mux2_Out_t ;
 	 //pc_mux2_sel_t = jalr_t ;
 	 always @( jalr_t )
 	   begin
@@ -157,25 +161,25 @@ module Single_Cycle_Processor(
 		end
 		
 		
-		assign next_addr_t = pc_mux2_Out_t ;
+		assign next_addr_t= pc_mux2_Out_t ;
 	
 	  
 	 // Implementation of Data Memory module 
-	 wire  d_mem_wr_en_t ;
 	 
-	 reg [1:0] d_mem_size ; 
+	 
+	 reg [4:0] d_mem_word_size ; 
 	 //reg [`REGISTER_WIDTH : 0]  d_mem_wr_data_t == ALU_Out_t;
 	 
-	 always @ ( d_mem_size_t ) begin 
-	   case (d_mem_size_t)
-               2'b00 : d_mem_size = 32 ; 
-               2'b10 : d_mem_size = 8;
-               default : d_mem_size = 16 ;
-           endcase    
-         end
-         parameter DATA_MEM_WORD_SIZE = d_mem_size ;
-         reg [DATA_MEM_WORD_SIZE-1 : 0 ] d_mem_rd_data_t;
-         Dist_RAM Dist_RAM_t( .d_mem_wr_en_t(we) , // .d_mem_size_t(d_mem_size), already taken care of using parameter
-                    .ALU_Out_t(a) ,.reg_data_2_t(d) , .d_mem_rd_data_t(spo) ,
-	            .clk_t(clk) );
+	// always @ ( d_mem_size_t ) begin 
+	 //  case (d_mem_size_t)
+     //          2'b00 : d_mem_word_size = 5'd32 ; 
+      //         2'b10 : d_mem_word_size = 5'd8;
+       //        default : d_mem_word_size = 5'd16 ;
+        //   endcase    
+         // end
+         parameter DATA_MEM_WORD_SIZE = 16 ;
+         wire [DATA_MEM_WORD_SIZE-1 : 0 ] d_mem_rd_data_t;
+         Dist_RAM Dist_RAM_t( .we(d_mem_wr_en_t) , // .d_mem_size_t(d_mem_size), already taken care of using parameter
+                    .a(ALU_Out_t) ,.d(reg_data_2_t) , .spo(d_mem_rd_data_t) ,
+	            .clk(clk_t) );
 	 endmodule
